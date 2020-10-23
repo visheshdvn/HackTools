540444de4f3bd048a36e9a78b605d3119a9f5b70
visheshdvn <vishesh.dh1@gmail.com>
2020-10-24 02:07:59 +0530

    macchanger improved and finalized

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env/ python3
-import subprocess, optparse
+import subprocess, optparse, re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -21,7 +21,28 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+
+def get_current_mac(interface):
+    command_output = subprocess.check_output(['ifconfig', interface])
+    command_output = command_output.decode('utf-8')
+
+    mac_address_search_result = re.search(r"(\w\w:){5}\w\w", command_output)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+        exit(0)
+
+
 options = get_arguments()
-
-
+current_mac = get_current_mac(options.interface)
+print('Current MAC = ', current_mac)
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to", current_mac)
+else:
+    print("[-] MAC address did not changed.")
